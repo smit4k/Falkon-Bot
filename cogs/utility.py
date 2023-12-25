c44729d315c70ae9eece31080925934aa23bdbfd
smit4k <92962589+smit4k@users.noreply.github.com>
2023-12-25 09:01:17 -0500

    Cleaned up

--- cogs/utility.py
+++ cogs/utility.py
@@ -50,12 +50,5 @@ class Utility(commands.Cog):
         usEmbed.set_footer(text = f'Requested by {ctx.author.name}', icon_url = ctx.author.display_avatar)
         await ctx.send(embed = usEmbed)
 
-    @commands.command()
-    async def ver(self, ctx):
-        verEmbed = discord.Embed(title = "BETA 0.02", color = 0x6B31A5, timestamp = datetime.now())
-        verEmbed.add_field(name = "**Upcoming Features:**", value = "* Steam Commands")
-        verEmbed.set_footer(text = f'Requested by {ctx.author.name}', icon_url = ctx.author.display_avatar)
-        await ctx.send(embed = verEmbed)
-
 async def setup(client):
     await client.add_cog(Utility(client))
