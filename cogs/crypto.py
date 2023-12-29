c704c6e41d0c013829e9182a123a54060794ce22
smit4k <92962589+smit4k@users.noreply.github.com>
2023-12-29 10:10:29 -0500

    Made the cryptoprice command

--- cogs/crypto.py
+++ cogs/crypto.py
@@ -12,6 +12,20 @@ class Crypto(commands.Cog):
     def __init__(self,client):
         self.client = client
 
+    @commands.command()
+    async def cryptoprice(self, ctx, *, crypto):
+        price = await self.get_Crypto_Price(crypto)
+
+        cryptoEmbed = discord.Embed(title = f"Price for {crypto.upper()}", color = 0x6B31A5, timestamp = datetime.now())
+        cryptoEmbed.add_field(name = f"{crypto.upper()} - USD", value = price, inline = False)
+        cryptoEmbed.set_footer(text = f'Requested by {ctx.author.name}', icon_url = ctx.author.display_avatar)
+        await ctx.send(embed = cryptoEmbed)
+
+    async def get_Crypto_Price(self,cryptocurrency):
+        response = requests.get(f"https://api.blockchain.com/v3/exchange/tickers/{cryptocurrency.upper()}-USD")
+        data = response.json()
+        price = data["price_24h"]
+        return f"**Price:** ${price}"
     
 
 async def setup(client):
