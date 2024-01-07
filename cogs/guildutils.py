bda5691dac94524bc4ca4595a2cf5b9edb797c9c
smit4k <92962589+smit4k@users.noreply.github.com>
2024-01-06 20:24:10 -0500

    Added onlinemembers command

--- cogs/guildutils.py
+++ cogs/guildutils.py
@@ -21,5 +21,17 @@ class GuildUtils(commands.Cog):
         mcountEmbed.set_footer(text = f'Requested by {ctx.author.name}', icon_url = ctx.author.display_avatar)
         await ctx.send(embed = mcountEmbed)
 
+    @commands.command()
+    async def onlinemembers(self, ctx):
+        online_count = sum(1 for member in ctx.guild.members if member.status == discord.Status.online)
+        idle_count = sum(1 for member in ctx.guild.members if member.status == discord.Status.idle)
+        dnd_count = sum(1 for member in ctx.guild.members if member.status == discord.Status.do_not_disturb)
+
+        onEmbed = discord.Embed(title = "Online Members", color = 0x6B31A5, timestamp = datetime.now())
+        onEmbed.add_field(name = "Statuses", value = f"**Online:** {online_count} members\n**Idle:** {idle_count} members \n**Do Not Disturb:** {dnd_count} members", inline = False)
+        onEmbed.add_field(name = "Total Users Online", value = online_count + idle_count + dnd_count, inline = False)
+        onEmbed.set_footer(text = f'Requested by {ctx.author.name}', icon_url = ctx.author.display_avatar)
+        await ctx.send(embed = onEmbed)
+
 async def setup(client):
     await client.add_cog(GuildUtils(client))
