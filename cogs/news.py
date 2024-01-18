ee6ea7d4e7ddcbbca0c9dffa30efd88c7c5771ff
Smit Patil <smit.patil.usa@gmail.com>
2024-01-18 13:28:04 -0500

    Added wsj news command

--- cogs/news.py
+++ cogs/news.py
@@ -35,6 +35,25 @@ class News(commands.Cog):
         newsEmbed.set_footer(text = f'Requested by {ctx.author.name}', icon_url = ctx.author.display_avatar)
         await ctx.send(embed = newsEmbed)
 
+    @commands.command()
+    async def wsj(self,ctx):
+        wsjTitle = await self.get_WSJ_Title()
+        wsjURL = await self.get_WSJ_URL()
+        source = await self.get_WSJ_Source()
+        imgurl = await self.get_WSJ_Image()
+        description = await self.get_WSJ_Description()
+        publishDate = await self.get_WSJ_PublishDate()
+
+        wsjEmbed = discord.Embed(title = wsjTitle, url = wsjURL, color = 0x6B31A5, timestamp = datetime.now())
+        wsjEmbed.add_field(name = "Source", value = source, inline = False)
+        wsjEmbed.add_field(name = "Publish Date", value = publishDate, inline = False)
+        wsjEmbed.add_field(name = "Description", value = description, inline = False)
+        wsjEmbed.set_image(url = imgurl)
+        wsjEmbed.set_footer(text = f'Requested by {ctx.author.name}', icon_url = ctx.author.display_avatar)
+        await ctx.send(embed = wsjEmbed)
+
+
+
     async def get_BusinessUS_Source(self):
         response = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={newskey}")
         data = response.json()
@@ -70,6 +89,44 @@ class News(commands.Cog):
         data = response.json()
         imgurl = data["articles"][0]["urlToImage"]
         return imgurl
+    
+    async def get_WSJ_Source(self):
+        response = requests.get(f"https://newsapi.org/v2/everything?domains=wsj.com&apiKey={newskey}")
+        data = response.json()
+        source = data["articles"][0]["source"]["name"]
+        author = data["articles"][0]["author"]
+        return f"{source}, by *{author}*"
+
+    async def get_WSJ_Title(self):
+        response = requests.get(f"https://newsapi.org/v2/everything?domains=wsj.com&apiKey={newskey}")
+        data = response.json()
+        title = data["articles"][0]["title"]
+        return title
+    
+    async def get_WSJ_Description(self):
+        response = requests.get(f"https://newsapi.org/v2/everything?domains=wsj.com&apiKey={newskey}")
+        data = response.json()
+        description = data["articles"][0]["description"]
+        return description
+    
+    async def get_WSJ_URL(self):
+        response = requests.get(f"https://newsapi.org/v2/everything?domains=wsj.com&apiKey={newskey}")
+        data = response.json()
+        url = data["articles"][0]["url"]
+        return url
+    
+    async def get_WSJ_PublishDate(self):
+        response = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={newskey}")
+        data = response.json()
+        publishedAt = data["articles"][0]["publishedAt"]
+        return publishedAt[0:10]
+    
+    async def get_WSJ_Image(self):
+        response = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={newskey}")
+        data = response.json()
+        imgurl = data["articles"][0]["urlToImage"]
+        return imgurl
+
         
 
 async def setup(client):
