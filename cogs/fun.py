61c8debab24732592d105b0d5a45501e44d905fd
Smit Patil <smit.patil.usa@gmail.com>
2024-05-15 16:02:40 -0400

    Added 8ball responses to a seperate text file - simplified

--- cogs/fun.py
+++ cogs/fun.py
@@ -9,26 +9,6 @@ from PIL import Image
 import io
 
 coinFlip = ["Heads", "Tails"]
-responses = ["It is certain.",
-"It is decidedly so.",
-"Without a doubt.",
-"Yes - definitely.",
-"You may rely on it.",
-"As I see it, yes.",
-"Most likely.",
-"Outlook good.",
-"Yes.",
-"Signs point to yes.",
-"Reply hazy, try again.",
-"Ask again later.",
-"Better not tell you now.",
-"Cannot predict now.",
-"Concentrate and ask again.",
-"Don't count on it.",
-"My reply is no.",
-"My sources say no.",
-"Outlook not so good.",
-"Very doubtful."]
 
 tz = timezone('EST')
 datetime.now(tz)
@@ -53,9 +33,10 @@ class Fun(commands.Cog):
 
     @commands.command(aliases = ["8ball"])
     async def eightball(self, ctx, *, question):
+        response = await self.getResponse()
         _8ballEmbed = discord.Embed(title = "8Ball", color = 0x6B31A5, timestamp = datetime.now())
         _8ballEmbed.add_field(name = "**Question:**", value = question, inline = False)
-        _8ballEmbed.add_field(name = "**Answer:**", value = random.choice(responses), inline = False)
+        _8ballEmbed.add_field(name = "**Answer:**", value = response, inline = False)
         _8ballEmbed.set_footer(text = f'Requested by {ctx.author.name}', icon_url = ctx.author.display_avatar)
         await ctx.send(embed = _8ballEmbed)
 
@@ -128,6 +109,19 @@ class Fun(commands.Cog):
         boEmbed.set_footer(text = f'Requested by {ctx.author.name}', icon_url = ctx.author.display_avatar)
         await ctx.send(embed = boEmbed)
 
+    async def getResponse(self):
+        file_path = "assets/responses.txt"
+        response = "";
+        try:
+            with open(file_path, "r", encoding="utf-8") as file:
+                lines = file.read().splitlines()
+                if lines:
+                    response = random.choice(lines)
+                else:
+                    response = "Error: File not found."
+        except Exception as e:
+            response = "An error occurred: {e}"
+        return response
 
     async def get_Joke(self):
         response = requests.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&type=single")
