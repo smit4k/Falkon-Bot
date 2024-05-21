a1ec3fc41729e0d4602e0b701c8c0ca3e051ebe7
Smit Patil <smit.patil.usa@gmail.com>
2024-05-20 22:09:58 -0400

    Added interval to stockprice command

--- cogs/stocks.py
+++ cogs/stocks.py
@@ -23,9 +23,12 @@ class Stocks(commands.Cog):
 
     @commands.command()
     async def stockprice(self,ctx,*,ticker):
+        ticker = ticker.upper()
         openPrice = await self.getCurrentOpenStockPrice(ticker)
+        interval = await self.getInterval(ticker)
         timeZone = await self.getTimeZone(ticker)
         stockEmbed = discord.Embed(title = ticker, color = 0x6B31A5, timestamp = datetime.now())
+        stockEmbed.add_field(name = "Interval", value = interval, inline = False)
         stockEmbed.add_field(name = "Time Zone", value = timeZone, inline = False)
         stockEmbed.add_field(name = "Open Price", value = openPrice, inline = False)
         stockEmbed.set_footer(text = f'Requested by {ctx.author.name}', icon_url = ctx.author.display_avatar) 
@@ -41,6 +44,12 @@ class Stocks(commands.Cog):
         open_price = time_series[latest_timestamp]["1. open"]
         return open_price
 
+    async def getInterval(self,ticker):
+        response = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={ticker}&interval=5min&apikey={KEY}")
+        data = response.json()
+        interval = data["Meta Data"]["4. Interval"]
+        return interval
+        
     async def getTimeZone(self, ticker):
         response = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={ticker}&interval=5min&apikey={KEY}")
         data = response.json()
